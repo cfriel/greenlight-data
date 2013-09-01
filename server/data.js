c47637379ff2ba6e40a10d6af45192ff426e8468
Chris Friel <cfriel@gmail.com>
2013-08-31 23:55:45 -0400

    creating greenlight data

--- server/data.js
+++ server/data.js
@@ -1,11 +1,11 @@
-var name = "profile";
+var name = "data";
 var version = "1.0";
 
-profile = function(){};
+data = function(){};
 
-profile.prototype = new profile();
+data.prototype = new data();
 
-profile.prototype.metadata = function()
+data.prototype.metadata = function()
 {
     
     return {
@@ -15,40 +15,9 @@ profile.prototype.metadata = function()
 
 
 Meteor.methods({
-  saveFile: function(blob, name, path, encoding) {
-      var path = cleanPath(path);
-      var fs = Npm.require('fs');
-      var name = cleanName(name || 'file');
-      var encoding = encoding || 'binary';
-      var chroot = Meteor.chroot || 'public';
-
-    // Clean up the path. Remove any initial and final '/' -we prefix them-,
-    // any sort of attempt to go to the parent directory '..' and any empty directories in
-    // between '/////' - which may happen after removing '..'
-    path = chroot + (path ? '/' + path + '/' : '/');
-    
-    // TODO Add file existance checks, etc...
-    fs.writeFile(path + name, blob, encoding, function(err) {
-      if (err) {
-        throw (new Meteor.Error(500, 'Failed to save file.', err));
-      } else {
-        Greenlight.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
-      }
-    }); 
- 
-    function cleanPath(str) {
-      if (str) {
-        return str.replace(/\.\./g,'').replace(/\/+/g,'').
-          replace(/^\/+/,'').replace(/\/+$/,'');
-      }
-    }
-    function cleanName(str) {
-      return str.replace(/\.\./g,'').replace(/\//g,'');
-    }
-  }
 });
 
-Greenlight.Packages.Profile = profile.prototype;
+Greenlight.Packages.Data = data.prototype;
     
-Greenlight.register_package(name, version, Greenlight.Packages.Profile);    
+Greenlight.register_package(name, version, Greenlight.Packages.Data);    
 
