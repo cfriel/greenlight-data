cdcdafea047648e9fe4777ad1dbcdedb7235386d
Chris Friel <cfriel@gmail.com>
2013-10-06 01:42:40 -0400

    data navigation improvements

--- server/data.js
+++ server/data.js
@@ -9,7 +9,7 @@ data.prototype.metadata = function()
 {
     
     return {
-	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a purus quis ligula varius aliquam id sit amet lacus. Ut quam tellus, aliquet vitae tortor blandit, rhoncus semper arcu. Suspendisse pretium dolor arcu, et semper ante aliquam non. In nec quam cursus, congue felis vitae, consectetur ipsum. Nullam nisl turpis, tempor vitae felis eleifend, fringilla pretium turpis. Aliquam egestas nibh tortor, eu iaculis nibh tincidunt sit amet. Aliquam auctor erat non tellus adipiscing fringilla. In porttitor mattis eros, et dictum nulla blandit non. Cras viverra velit vel turpis imperdiet commodo. Maecenas non leo at leo feugiat aliquam. Fusce semper molestie ligula, et cursus sapien volutpat non. Vivamus leo felis, cursus ut nunc et, porttitor facilisis orci. Donec vehicula vehicula ligula, vel rhoncus velit lobortis non."
+	description : "The data package provides functionality for integration with other systems.  It allows users to define endpoints, transforms, streams, and datasets, populating the system with data in realtime.  All data sourced in this manner is automatically indexed and made available immediately."
     };
 }();
 
